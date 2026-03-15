bb57181278fb6d657082f5da8f31ff9acbc35d34
Iyyappasaran.S <iyyappaalpha@gmail.com>
2026-03-15 18:53:46 +0530

    Update playback.py

--- playback.py
+++ playback.py
@@ -1,3 +1,3 @@
 # Replacing each space with "..." three periods with user's input
 text = input("Enter text: ")
-print("Text after conversion:",text.replace(" ","..."))
+print(text.replace(" ","..."))
