f0dc9b7013bb489caa2340a29022b07194c31deb
Iyyappasaran.S <iyyappaalpha@gmail.com>
2026-04-28 00:15:21 +0530

    Updated twttr.py due to Lecture 5

--- twttr.py
+++ twttr.py
@@ -1,7 +1,14 @@
 # Implement a program that prompts the user for string and outputs the strin with vowels removed
-text = input("Input: ")
-result = ""
-for n in text:
-    if n not in "AEIOUaeiou":
-        result += n
-print("Output:",result)
+def main():
+    text = input("Input: ")
+    print(shorten(text))
+
+def shorten(word):
+    result = ""
+    for n in word:
+        if n not in "AEIOUaeiou":
+            result += n
+    return f"{result}"
+
+if __name__ == "__main__":
+    main()
