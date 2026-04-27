bab666693e6c86d99bf67ab1740aef64600044bb
Iyyappasaran.S <iyyappaalpha@gmail.com>
2026-04-28 00:16:55 +0530

    Updated bank.py due to Lecture 5

--- bank.py
+++ bank.py
@@ -1,8 +1,16 @@
 # Implement a program that prompts user for greeting and do operation with conditionals
-text = input("Greeting: ").strip().lower()
-if text.startswith("hello"):
-    print("$0")
-elif text.startswith("h"):
-    print("$20")
-else:
-    print("$100")
+def main():
+    text = input("Greeting: ")
+    print(f"${value(text)}")
+
+def value(greeting):
+    greeting = greeting.strip().lower()
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
+    
+if __name__ == "__main__":
+    main()
